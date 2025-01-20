1464135b591e110b4f64cc93e2a74c9348d40b9a
MArTAUTAD <157921594+MArTAUTAD@users.noreply.github.com>
2025-01-20 22:24:27 +0100

    Update page.js

--- Practica2/src/app/pagesInfo/clients/page.js
+++ Practica2/src/app/pagesInfo/clients/page.js
@@ -1,48 +1,81 @@
-"use client";
-import ClientsList from "@/components/ClientsList";
-import { useEffect, useState } from "react";
-import { getFetch } from "@/utils/handlerequests";
-import Link from "next/link";
-import CreateClient from "./create/page";
+"use client"; 
 
-export default function Clients() {
-  const [clients, setClients] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
 
-  // Función para obtener la lista de clientes
-  useEffect(() => {
-    async function fetchClients() {
-      try {
-        const data = await getFetch("api/client", null, "GET", {});
-        setClients(data || []);
-      } catch (err) {
-        setError(err.message || "Error al obtener los clientes.");
-      } finally {
-        setLoading(false);
+import { useParams } from 'next/navigation';
+import { useSearchParams } from "next/navigation";
+import { getFetch } from '@/utils/handlerequests';
+import { useRouter } from 'next/navigation'; // Usa next/navigation para la navegación
+import { useEffect, useState } from 'react';
+
+
+export default function ClientDetail (){
+    const { id } = useParams(); // Extraer el ID desde los parámetros dinámicos de la URL
+    const [client, setClient] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    console.log(id)
+    useEffect(() => {
+      if (!id) {
+        console.error("El ID no está disponible en los parámetros.");
+        return;
       }
-    }
+  
+      const fetchClientDetails = async () => {
+        setLoading(true);
+        try {
+          const data = await getFetch(`api/client/${id}`, null, 'GET', {});
+          setClient(data);
+        } catch (err) {
+          console.error("Error al obtener los detalles del cliente:", err);
+          setError(err.message);
+        // } finally {
+        //   setLoading(false);
+        }
+      };
+  
+      fetchClientDetails();
+    }, [id]);
 
-    fetchClients();
-  }, []);
+    console.log(client)
 
-  if (loading) return <p>Cargando...</p>;
+
+    const fields = [
+      { name: "name", type: "text", label: "Nombre del Cliente" , value:client.name},
+      { name: "cif", type: "text", label: "CIF", value:client.cif },
+      // { name: "email", type: "email", label: "Correo Electrónico", value:client.email },
+      // { name: "phone", type: "number", label: "Teléfono", value:client.name },
+      { name: "address.street", type: "text", label: "Calle" , value:client.address.street},
+      { name: "address.number", type: "number", label: "Número", value:client.address.number },
+      { name: "address.postal", type: "number", label: "Código Postal", value:client.address.postal },
+      { name: "address.city", type: "text", label: "Ciudad", value:client.address.city },
+      { name: "address.province", type: "text", label: "Provincia", value:client.address.province },
+  ];
+
+
+  if (error) {
+    return (
+      <div>
+        <p>Error: {error}</p>
+        <button onClick={() => router.push('/pagesInfo/clients')}>
+          Volver a la lista
+        </button>
+      </div>
+    );
+  }
+
 
   return (
-    <div>
-      {error && <p className="error">{error}</p>}
-
-      {Array.isArray(clients) && clients.length === 0 ? (
-        <div className="no-clients">
-          <h2>Crea tu primer Cliente</h2>
-          <p>Para poder generar Albaranes digitales</p>
-          <Link href="/pagesInfo/clients/create">
-            <button className="">¡Sí, vamos!</button>
-          </Link>
-        </div>
-      ) : (
-        <ClientsList clients={clients} setClients={setClients} />
-      )}
+    <div style={{ maxWidth: '600px', margin: '0 auto' }}>
+      <h1>Detalles del Cliente</h1>
+      <div>
+        {fields.map((detail)=>(
+            <p><strong>{detail.label}:</strong> {detail.value}</p>
+        ))}
+      </div>
+      <button onClick={() => router.push('/pagesInfo/clients')}>
+        Volver a la lista
+      </button>
     </div>
   );
-}
+};
